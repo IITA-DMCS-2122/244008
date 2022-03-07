7050f652740ed48baa65ab88baae54c07da8df58
jakub flaszka <244008@edu.p.lodz.pl>
2022-03-07 21:50:14 +0100

    spring init

---
+++ src/test/java/pl/lodz/it/jf/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.it.jf.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
